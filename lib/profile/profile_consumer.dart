d9dcf04a1b1aa4a2e60b0c278af56bd7e1dcba99
Moricchi <ptnadia337@gmail.com>
2024-06-05 12:24:16 +0800

    profile owner

--- lib/profile/profile_consumer.dart
+++ lib/profile/profile_consumer.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:berry_happy/components/assets_image_widget.dart';
 
 class ConsumerProfile extends StatefulWidget {
   const ConsumerProfile({super.key});
