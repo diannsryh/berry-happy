fb4190bad9a9b32814496c8bf2b27eca96651be7
Moricchi <ptnadia337@gmail.com>
2024-06-05 12:41:58 +0800

    dashboard customer and owner

--- lib/dashboard/dashboard_consumer.dart
+++ lib/dashboard/dashboard_consumer.dart
@@ -1,11 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-// import 'package:google_fonts/google_fonts.dart';
-// import 'package:berry_happy/components/assets_image_widget.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DashboardConsumer extends StatefulWidget {
-  const DashboardConsumer({super.key});
+  const DashboardConsumer({Key? key}) : super(key: key);
 
   @override
   State<DashboardConsumer> createState() => _DashboardConsumerState();
@@ -20,9 +17,9 @@ class _DashboardConsumerState extends State<DashboardConsumer> {
         child: Column(
           children: [
             const SizedBox(height: 100),
-            const Divider(
-              color: Colors.white,
-            ),
+            const Divider(color: Colors.white),
+
+            // Search Bar
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 18),
               child: Container(
@@ -35,81 +32,47 @@ class _DashboardConsumerState extends State<DashboardConsumer> {
                   decoration: InputDecoration(
                     fillColor: const Color.fromARGB(255, 224, 224, 224),
                     hintText: 'Search...',
-                    hintStyle: TextStyle(fontSize: 16),
+                    hintStyle: GoogleFonts.poppins(fontSize: 16),
                     border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: const BorderSide(
-                            color: Color.fromARGB(255, 122, 122, 122))),
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: const BorderSide(
+                          color: Color.fromARGB(255, 122, 122, 122)),
+                    ),
                     prefixIcon: const Icon(Icons.search, size: 35),
                   ),
                 ),
               ),
             ),
+
             const Divider(color: Colors.white),
-            const Padding(
-              padding: EdgeInsets.only(top: 5, bottom: 10, left: 65, right: 65),
+
+            // Total Order Display
+            Padding(
+              padding:
+                  const EdgeInsets.only(top: 5, bottom: 5, left: 65, right: 65),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Column(
                     children: [
                       Text(
                         "Rp.69.000",
-                        style: TextStyle(fontWeight: FontWeight.bold),
+                        style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.bold, fontSize: 20),
                       ),
-                      Text("Total Order"),
+                      Text(
+                        "Total Order",
+                        style: GoogleFonts.poppins(
+                            fontSize: 16, fontWeight: FontWeight.normal),
+                      ),
                     ],
                   ),
-                  Column(
-                    children: [
-                      Text(
-                        "15-30 min",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Text("Delivery Time")
-                    ],
-                  )
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
     );
   }
 }
-
-
-// import 'package:flutter/material.dart';
-
-// class DashboardConsumer extends StatefulWidget {
-//   const DashboardConsumer({super.key});
-
-//   @override
-//   State<DashboardConsumer> createState() => _DashboardConsumerState();
-// }
-
-// class _DashboardConsumerState extends State<DashboardConsumer> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return SingleChildScrollView(
-//       child: Column(
-//         children: [
-//           Divider(),
-//           Padding(
-//               padding: const EdgeInsets.symmetric(horizontal: 14),
-//               child: TextFormField(
-//                 decoration: const InputDecoration(
-//                     border: OutlineInputBorder(),
-//                     prefixIcon: Padding(
-//                       padding: EdgeInsets.only(left: 16, right: 20),
-//                       child: Icon(Icons.search),
-//                     ),
-//                     hintText: 'Find Courses'),
-//               )),
-//           Divider(),
-//         ],
-//       ),
-//     );
-//   }
-// }
