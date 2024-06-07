cca8d7b6ab8d6103da79f732926e6375b8ee756e
qieiu <ammuliarizqie@gmail.com>
2024-06-07 15:08:56 +0800

    new endpoint

--- lib/endpoints/endpoints.dart
+++ lib/endpoints/endpoints.dart
@@ -1,8 +1,8 @@
 class Endpoints {
   static const String host = '';
-  static const String urlUAS = 'http://10.0.2.2:5000';
+  static const String urlUAS = 'http://192.168.199.120:5000';
 
-  static const String baseURL = "http://10.0.2.2:5000/api/v1/menu";
+  static const String baseURL = "http://192.168.199.120:5000/api/v1/menu";
 
   static const String menu = "$baseURL/read";
 
