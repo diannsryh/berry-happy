71631a0a45870c6a47820bb141b1929e27542e58
lialialiashymfony <151517394+lialialiashymfony@users.noreply.github.com>
2024-06-07 17:23:16 +0800

    mau tampil owner dan cust

--- lib/dashboard/dashboard_owner.dart
+++ lib/dashboard/dashboard_owner.dart
@@ -3,6 +3,7 @@ import 'package:berry_happy/endpoints/endpoints.dart';
 import 'package:berry_happy/menu/add_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:berry_happy/services/data_service.dart';
 
 class DashboardOwner extends StatefulWidget {
   const DashboardOwner({Key? key}) : super(key: key);
@@ -13,6 +14,11 @@ class DashboardOwner extends StatefulWidget {
 
 class _DashboardOwnerState extends State<DashboardOwner> {
   Future<List<Menu>>? _menu;
+  @override
+  void initState() {
+    super.initState();
+    _menu = DataService.fetchMenu();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -112,7 +118,7 @@ class _DashboardOwnerState extends State<DashboardOwner> {
                               fontWeight: FontWeight.bold, fontSize: 20),
                         ),
                         Text(
-                          "Total Order",
+                          "Total Income",
                           style: GoogleFonts.poppins(
                               fontSize: 16, fontWeight: FontWeight.normal),
                         ),
@@ -121,73 +127,89 @@ class _DashboardOwnerState extends State<DashboardOwner> {
                   ],
                 ),
               ),
-              FutureBuilder<List<Menu>>(
-                  future: _menu,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      final data = snapshot.data!;
-                      return ListView.builder(
-                          shrinkWrap: true,
-                          physics: const NeverScrollableScrollPhysics(),
-                          itemCount: data.length,
-                          itemBuilder: (context, index) {
-                            final item = data[index];
-                            return ListTile(
-                              title: Column(children: [
-                                Text('Menu Name : ${item.menuName}',
-                                    style: GoogleFonts.poppins(
-                                      fontSize: 14,
-                                      color:
-                                          const Color.fromARGB(255, 36, 31, 31),
-                                      fontWeight: FontWeight.normal,
-                                    )),
-                                Text('Deskripsi : ${item.descMenu}',
-                                    style: GoogleFonts.poppins(
-                                      fontSize: 14,
-                                      color:
-                                          const Color.fromARGB(255, 36, 31, 31),
-                                      fontWeight: FontWeight.normal,
-                                    )),
-                                Text('Price: ${item.menuPrice}',
-                                    style: GoogleFonts.poppins(
-                                      fontSize: 14,
-                                      color:
-                                          const Color.fromARGB(255, 36, 31, 31),
-                                      fontWeight: FontWeight.normal,
-                                    )),
-                              ]),
-                              subtitle: item.imageUrl != null
-                                  ? Column(
-                                      children: [
-                                        Row(
-                                          children: [
-                                            ClipRRect(
-                                              borderRadius:
-                                                  BorderRadius.circular(10),
-                                              child: Image.network(
-                                                fit: BoxFit.fitWidth,
-                                                width: 100,
-                                                Uri.parse(
-                                                        '${Endpoints.urlUAS}/static/storages/${item.imageUrl!}')
-                                                    .toString(),
-                                                errorBuilder: (context, error,
-                                                        stackTrace) =>
-                                                    const Icon(Icons
-                                                        .error), // Display error icon if image fails to load
-                                              ),
-                                            ),
-                                          ],
+              // Menu List Container
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+                margin: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: FutureBuilder<List<Menu>>(
+                    future: _menu,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        final data = snapshot.data!;
+                        return ListView.builder(
+                            shrinkWrap: true,
+                            physics: NeverScrollableScrollPhysics(),
+                            itemCount: data.length,
+                            itemBuilder: (context, index) {
+                              final item = data[index];
+                              return Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text('${item.menuName}',
+                                              style: GoogleFonts.poppins(
+                                                fontSize: 20,
+                                                color: const Color.fromARGB(
+                                                    255, 36, 31, 31),
+                                                fontWeight: FontWeight.bold,
+                                              )),
+                                          Text('Rp. ${item.menuPrice}',
+                                              style: GoogleFonts.poppins(
+                                                fontSize: 16,
+                                                color: const Color.fromARGB(
+                                                    255, 36, 31, 31),
+                                                fontWeight: FontWeight.bold,
+                                              )),
+                                          Text('${item.descMenu}',
+                                              style: GoogleFonts.poppins(
+                                                fontSize: 14,
+                                                color: const Color.fromARGB(
+                                                    255, 36, 31, 31),
+                                                fontWeight: FontWeight.normal,
+                                              )),
+                                        ],
+                                      ),
+                                    ),
+                                    if (item.imageUrl != null)
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 16.0),
+                                        child: ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          child: Image.network(
+                                            fit: BoxFit.cover,
+                                            width: 100,
+                                            height: 100,
+                                            Uri.parse(
+                                                    '${Endpoints.urlUAS}/static/storages/${item.imageUrl!}')
+                                                .toString(),
+                                            errorBuilder:
+                                                (context, error, stackTrace) =>
+                                                    const Icon(Icons.error),
+                                          ),
                                         ),
-                                      ],
-                                    )
-                                  : null,
-                            );
-                          });
-                    } else if (snapshot.hasError) {
-                      return Center(child: Text('${snapshot.error}'));
-                    }
-                    return const Center(child: CircularProgressIndicator());
-                  })
+                                      ),
+                                  ],
+                                ),
+                              );
+                            });
+                      } else if (snapshot.hasError) {
+                        return Center(child: Text('${snapshot.error}'));
+                      }
+                      return const Center(child: CircularProgressIndicator());
+                    }),
+              ),
             ],
           ),
         ),
